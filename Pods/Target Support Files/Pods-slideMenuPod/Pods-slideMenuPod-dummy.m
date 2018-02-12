2d48b074b9d4f5933d85b6b67ea1f58bd1a2e8c6
Sujeet Kumar <sujeetk@fractalanalytics.com>
2018-02-12 19:22:02 +0530

    first commit

---
+++ Pods/Target Support Files/Pods-slideMenuPod/Pods-slideMenuPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_slideMenuPod : NSObject
+@end
+@implementation PodsDummy_Pods_slideMenuPod
+@end
---
+++ slideMenuPod/slideMenuPod.h
@@ -0,0 +1,19 @@
+//
+//  slideMenuPod.h
+//  slideMenuPod
+//
+//  Created by Sujeet.Kumar on 2/12/18.
+//  Copyright © 2018 Sujeet.Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for slideMenuPod.
+FOUNDATION_EXPORT double slideMenuPodVersionNumber;
+
+//! Project version string for slideMenuPod.
+FOUNDATION_EXPORT const unsigned char slideMenuPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <slideMenuPod/PublicHeader.h>
+
+
